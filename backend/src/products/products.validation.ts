ae84ad2ecc4e165f6bb023a0c0da7519fa579a82
abdelrahman-sherif55 <boodyshlby55@gmail.com>
2024-12-06 01:25:53 +0200

    complete localization

--- backend/src/products/products.validation.ts
+++ backend/src/products/products.validation.ts
@@ -1,4 +1,4 @@
-import { body, param } from "express-validator";
+import {body, param} from "express-validator";
 import validatorMiddleware from "../middlewares/validator.middleware";
 import categoriesSchema from "../categories/categories.schema";
 import subcategoriesSchema from "../subcategories/subcategories.schema";
@@ -7,81 +7,78 @@ class SubcategoriesValidation {
 
     createOne = [
         body('name')
-            .notEmpty().withMessage('product name is required')
-            .isLength({ min: 2, max: 50 }).withMessage('invalid length'),
+            .notEmpty().withMessage((val, {req}) => req.__('validation_field'))
+            .isLength({min: 2, max: 50}).withMessage((val, {req}) => req.__('validation_length_short')),
         body('description')
-            .notEmpty().withMessage('description is required')
-            .isLength({ min: 2, max: 500 }).withMessage('invalid length'),
+            .notEmpty().withMessage((val, {req}) => req.__('validation_field'))
+            .isLength({min: 2, max: 500}).withMessage((val, {req}) => req.__('validation_length_long')),
         body('price')
-            .notEmpty().withMessage('description is required')
-            .isFloat({ min: 1, max: 10000000 }).withMessage('invalid value'),
+            .notEmpty().withMessage((val, {req}) => req.__('validation_field'))
+            .isFloat({min: 1, max: 10000000}).withMessage((val, {req}) => req.__('validation_value')),
         body('quantity').optional()
-            .isInt({ min: 1, max: 10000000 }).withMessage('invalid value'),
+            .isInt({min: 1, max: 10000000}).withMessage((val, {req}) => req.__('validation_value')),
         body('discount').optional()
-            .isFloat({ min: 1, max: 100 }).withMessage('invalid value')
-            .custom((val, { req }) => {
+            .isFloat({min: 1, max: 100}).withMessage((val, {req}) => req.__('validation_value'))
+            .custom((val, {req}) => {
                 req.body.priceAfterDiscount = req.body.price - (req.body.price * val / 100)
                 return true;
             }),
         body('category')
-            .notEmpty().withMessage('category is required')
-            .isMongoId().withMessage('invalid id')
-            .custom(async (val: string) => {
+            .notEmpty().withMessage((val, {req}) => req.__('validation_field'))
+            .isMongoId().withMessage((val, {req}) => req.__('invalid_id'))
+            .custom(async (val: string, {req}) => {
                 const category = await categoriesSchema.findById(val);
-                if (!category) throw new Error(`Category does not exist`);
+                if (!category) throw new Error(`${req.__('validation_value')}`);
                 return true;
             }),
         body('subcategory')
-            .notEmpty().withMessage('category is required')
-            .isMongoId().withMessage('invalid id')
-            .custom(async (val: string, { req }) => {
+            .notEmpty().withMessage((val, {req}) => req.__('validation_field'))
+            .isMongoId().withMessage((val, {req}) => req.__('invalid_id'))
+            .custom(async (val: string, {req}) => {
                 const subcategory = await subcategoriesSchema.findById(val);
-                if (!subcategory) throw new Error(`Category does not exist`);
-                if (subcategory.category._id!.toString() !== req.body.category.toString()) throw new Error(`Subcategory not belong to this Category`);
+                if (!subcategory || subcategory.category._id!.toString() !== req.body.category.toString()) throw new Error(`${req.__('validation_value')}`);
                 return true;
             }),
         validatorMiddleware
     ]
     updateOne = [
-        param('id').isMongoId().withMessage('invalid id'),
-        body('name').optional().isLength({ min: 2, max: 50 }).withMessage('invalid length'),
-        body('description')
-            .optional()
-            .isLength({ min: 2, max: 500 }).withMessage('invalid length'),
-        body('price')
-            .optional()
-            .isFloat({ min: 1, max: 10000000 }).withMessage('invalid value'),
+        param('id').isMongoId().withMessage((val, {req}) => req.__('invalid_id')),
+        body('name').optional()
+            .isLength({min: 2, max: 50}).withMessage((val, {req}) => req.__('validation_length_short')),
+        body('description').optional()
+            .isLength({min: 2, max: 500}).withMessage((val, {req}) => req.__('validation_length_long')),
+        body('price').optional()
+            .isFloat({min: 1, max: 10000000}).withMessage((val, {req}) => req.__('validation_value')),
         body('quantity').optional()
-            .isInt({ min: 1, max: 10000000 }).withMessage('invalid value'),
+            .isInt({min: 1, max: 10000000}).withMessage((val, {req}) => req.__('validation_value')),
         body('discount').optional()
-            .isFloat({ min: 1, max: 100 }).withMessage('invalid value')
-            .custom((val, { req }) => {
+            .isFloat({min: 1, max: 100}).withMessage((val, {req}) => req.__('validation_value'))
+            .custom((val, {req}) => {
                 req.body.priceAfterDiscount = req.body.price - (req.body.price * val / 100)
                 return true;
             }),
         body('category').optional()
-            .isMongoId().withMessage('invalid id')
-            .custom(async (val: string) => {
+            .isMongoId().withMessage((val, {req}) => req.__('invalid_id'))
+            .custom(async (val: string, {req}) => {
                 const category = await categoriesSchema.findById(val);
-                if (!category) throw new Error(`Category does not exist`);
+                if (!category) throw new Error(`${req.__('validation_value')}`);
                 return true;
             }),
         body('subcategory').optional()
-            .isMongoId().withMessage('invalid id')
-            .custom(async (val: string, { req }) => {
+            .isMongoId().withMessage((val, {req}) => req.__('invalid_id'))
+            .custom(async (val: string, {req}) => {
                 const subcategory = await subcategoriesSchema.findById(val);
-                if (!subcategory) throw new Error(`Category does not exist`);
-                if (subcategory.category._id!.toString() !== req.body.category.toString()) throw new Error(`Subcategory not belong to this Category`);
+                if (!subcategory || subcategory.category._id!.toString() !== req.body.category.toString()) throw new Error(`${req.__('validation_value')}`);
                 return true;
             }),
         validatorMiddleware
     ]
     getOne = [
-        param('id').isMongoId().withMessage('invalid id'),
+        param('id').isMongoId().withMessage((val, {req}) => req.__('invalid_id')),
         validatorMiddleware
     ]
     deleteOne = [
-        param('id').isMongoId().withMessage('invalid id'),
+        param('id').isMongoId().withMessage((val, {req}) => req.__('invalid_id')),
         validatorMiddleware
     ]
 }
