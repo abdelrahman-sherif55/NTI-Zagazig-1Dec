377902a82d2b70ff8c57d32e43e4dbf5f4d3d3e2
abdelrahman-sherif55 <boodyshlby55@gmail.com>
2024-12-16 15:02:35 +0200

    test payment

--- backend/src/orders/payment.route.ts
+++ backend/src/orders/payment.route.ts
@@ -1,10 +1,7 @@
 import {Router} from 'express';
 import ordersService from "./orders.service";
-import authService from "../auth/auth.service";
 
 const paymentRouter: Router = Router();
-
-paymentRouter.use(authService.protectedRoutes, authService.checkActive);
 
 paymentRouter.post('/', ordersService.createOnlineOrder);
 
