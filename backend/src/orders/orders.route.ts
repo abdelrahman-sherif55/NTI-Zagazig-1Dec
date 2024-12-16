de13f41ced2d295fd60e9ac94587824e71193f23
abdelrahman-sherif55 <boodyshlby55@gmail.com>
2024-12-16 14:11:16 +0200

    orders

---
+++ backend/src/orders/orders.route.ts
@@ -0,0 +1,16 @@
+import {Router} from 'express';
+import ordersService from "./orders.service";
+import authService from "../auth/auth.service";
+
+const ordersRouter: Router = Router();
+
+ordersRouter.use(authService.protectedRoutes, authService.checkActive);
+
+ordersRouter.route('/')
+    .get(ordersService.filterOrders, ordersService.getAll)
+    .post(authService.allowedTo('user'), ordersService.createCashOrder);
+
+ordersRouter.put('/:id/deliver', authService.allowedTo('admin', 'employee'), ordersService.deliverOrder);
+ordersRouter.put('/:id/pay', authService.allowedTo('admin', 'employee'), ordersService.payOrder);
+
+export default ordersRouter;
